89c59831cada59a3537729d8428d35f14a55b202
Nikhil Prabhu <nikhilprabhu00@gmail.com>
2021-07-26 09:23:40 +0000

    Initialize backend app

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "App initialized.",
+		})
+	})
+
+	r.Run()
+}
