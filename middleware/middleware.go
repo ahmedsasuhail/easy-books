c97718fedc233aaba39e3bf5bbc09c96574e49c6
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2021-09-07 06:48:25 +0000

    Add CORS middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -10,6 +10,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// CORSMiddleware enables Cross-Origin requests.
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 // ValidateJWT validates a provided JWT authentication token.
 func ValidateJWT(secret, issuer string) gin.HandlerFunc {
 	return func(c *gin.Context) {
--- routes/routes.go
+++ routes/routes.go
@@ -3,13 +3,14 @@ package routes
 
 import (
 	"github.com/ahmedsasuhail/easy-books/controllers"
+	"github.com/ahmedsasuhail/easy-books/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 // Get returns a gin router with the configured routes and router groups.
 func Get() *gin.Engine {
-	// TODO: use custom middleware instead of default.
 	router := gin.Default()
+	router.Use(middleware.CORSMiddleware())
 
 	router.GET("/", controllers.Forbidden)
 
