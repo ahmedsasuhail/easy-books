f6364ccaf640c9f795188f90aa255fb462e52bec
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2021-11-07 14:03:52 +0000

    Fix incorrect total matched records count

--- controllers/inventory_crud.go
+++ controllers/inventory_crud.go
@@ -249,12 +249,15 @@ func GetInventoryByPurchaseID(c *gin.Context) {
 	}
 
 	successResponse(c, http.StatusOK, "", map[string]interface{}{
-		"page":                pagination.Page,
-		"page_limit":          pagination.PageLimit,
-		"order_by":            pagination.OrderBy,
-		"sort_order":          pagination.SortOrder,
-		"total_count":         pgClient.Find(&records).RowsAffected,
-		"records":             filteredRecords,
-		"total_matched_count": len(filteredRecords),
+		"page":        pagination.Page,
+		"page_limit":  pagination.PageLimit,
+		"order_by":    pagination.OrderBy,
+		"sort_order":  pagination.SortOrder,
+		"total_count": pgClient.Find(&records).RowsAffected,
+		"records":     filteredRecords,
+		"total_matched_count": pgClient.Where(
+			"purchase_id = ?",
+			record.PurchaseID,
+		).Find(&records),
 	})
 }
