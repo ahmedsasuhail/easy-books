ba028407fd6f3f0b2669ebc17b907ffe6964fb9b
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2021-11-12 10:59:28 +0000

    Hardcode Heroku app base URL

--- src/frontend/src/utils/axiosInstance.js
+++ src/frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL || "http://localhost:8080/eb/",
+  baseURL: process.env.REACT_APP_SERVER_URL || "https://easy-books.herokuapp.com/eb/",
   headers: {
     "Content-Type": "application/json",
   },
