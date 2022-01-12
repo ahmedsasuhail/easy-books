acf702e86d7eb386d5a564be4bc8f6790318ef2c
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2022-01-12 14:55:54 +0000

    Setup backend connection pool

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,9 +1,22 @@
 #[macro_use]
 extern crate rocket;
 
-mod routes;
+#[macro_use]
+extern crate diesel;
+
+use rocket_sync_db_pools::database;
+
+pub mod models;
+pub mod routes;
+pub mod schema;
+
+// Primary backend database connection pool.
+#[database("db")]
+pub struct Database(diesel::PgConnection);
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![routes::index])
+    rocket::build()
+        .attach(Database::fairing())
+        .mount("/", routes![routes::index])
 }
